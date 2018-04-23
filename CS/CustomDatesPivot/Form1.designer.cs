20f16b00baadf8ee908c4373d2d44516a83c90f5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/CustomDatesPivot/Form1.designer.cs
@@ -0,0 +1,246 @@
+﻿namespace CustomDatesPivot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pivotGridControl1 = new DevExpress.XtraPivotGrid.PivotGridControl();
+            this.pivotGridField1 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField2 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField3 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField4 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.pivotGridField5 = new DevExpress.XtraPivotGrid.PivotGridField();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.dateEdit1 = new DevExpress.XtraEditors.DateEdit();
+            this.dateEdit2 = new DevExpress.XtraEditors.DateEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.comboBoxEdit1 = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.VistaTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties.VistaTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pivotGridControl1
+            // 
+            this.pivotGridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pivotGridControl1.Fields.AddRange(new DevExpress.XtraPivotGrid.PivotGridField[] {
+            this.pivotGridField1,
+            this.pivotGridField2,
+            this.pivotGridField3,
+            this.pivotGridField4,
+            this.pivotGridField5});
+            this.pivotGridControl1.Location = new System.Drawing.Point(0, 0);
+            this.pivotGridControl1.Name = "pivotGridControl1";
+            this.pivotGridControl1.Size = new System.Drawing.Size(831, 452);
+            this.pivotGridControl1.TabIndex = 0;
+            this.pivotGridControl1.CustomFieldValueCells += new DevExpress.XtraPivotGrid.PivotCustomFieldValueCellsEventHandler(this.pivotGridControl1_CustomFieldValueCells);
+            // 
+            // pivotGridField1
+            // 
+            this.pivotGridField1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea;
+            this.pivotGridField1.AreaIndex = 0;
+            this.pivotGridField1.FieldName = "Name";
+            this.pivotGridField1.Name = "pivotGridField1";
+            // 
+            // pivotGridField2
+            // 
+            this.pivotGridField2.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea;
+            this.pivotGridField2.AreaIndex = 0;
+            this.pivotGridField2.FieldName = "Value";
+            this.pivotGridField2.Name = "pivotGridField2";
+            // 
+            // pivotGridField3
+            // 
+            this.pivotGridField3.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.pivotGridField3.AreaIndex = 2;
+            this.pivotGridField3.Caption = "Month";
+            this.pivotGridField3.FieldName = "Date";
+            this.pivotGridField3.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateMonth;
+            this.pivotGridField3.Name = "pivotGridField3";
+            this.pivotGridField3.UnboundFieldName = "pivotGridField3";
+            // 
+            // pivotGridField4
+            // 
+            this.pivotGridField4.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.pivotGridField4.AreaIndex = 0;
+            this.pivotGridField4.Caption = "Year";
+            this.pivotGridField4.FieldName = "Date";
+            this.pivotGridField4.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear;
+            this.pivotGridField4.Name = "pivotGridField4";
+            this.pivotGridField4.UnboundFieldName = "pivotGridField4";
+            // 
+            // pivotGridField5
+            // 
+            this.pivotGridField5.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea;
+            this.pivotGridField5.AreaIndex = 1;
+            this.pivotGridField5.Caption = "Quarter";
+            this.pivotGridField5.FieldName = "Date";
+            this.pivotGridField5.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateQuarter;
+            this.pivotGridField5.Name = "pivotGridField5";
+            this.pivotGridField5.UnboundFieldName = "pivotGridField5";
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.FixedPanel = DevExpress.XtraEditors.SplitFixedPanel.Panel2;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.pivotGridControl1);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.labelControl3);
+            this.splitContainerControl1.Panel2.Controls.Add(this.labelControl2);
+            this.splitContainerControl1.Panel2.Controls.Add(this.labelControl1);
+            this.splitContainerControl1.Panel2.Controls.Add(this.comboBoxEdit1);
+            this.splitContainerControl1.Panel2.Controls.Add(this.simpleButton1);
+            this.splitContainerControl1.Panel2.Controls.Add(this.dateEdit2);
+            this.splitContainerControl1.Panel2.Controls.Add(this.dateEdit1);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(1053, 452);
+            this.splitContainerControl1.SplitterPosition = 217;
+            this.splitContainerControl1.TabIndex = 1;
+            this.splitContainerControl1.Text = "splitContainerControl1";
+            // 
+            // dateEdit1
+            // 
+            this.dateEdit1.EditValue = null;
+            this.dateEdit1.Location = new System.Drawing.Point(12, 32);
+            this.dateEdit1.Name = "dateEdit1";
+            this.dateEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit1.Properties.VistaTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.dateEdit1.Size = new System.Drawing.Size(193, 20);
+            this.dateEdit1.TabIndex = 0;
+            // 
+            // dateEdit2
+            // 
+            this.dateEdit2.EditValue = null;
+            this.dateEdit2.Location = new System.Drawing.Point(12, 77);
+            this.dateEdit2.Name = "dateEdit2";
+            this.dateEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.dateEdit2.Properties.VistaTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.dateEdit2.Size = new System.Drawing.Size(193, 20);
+            this.dateEdit2.TabIndex = 1;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 170);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(193, 23);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "Create Custom Dates";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // comboBoxEdit1
+            // 
+            this.comboBoxEdit1.EditValue = "Month";
+            this.comboBoxEdit1.Location = new System.Drawing.Point(12, 135);
+            this.comboBoxEdit1.Name = "comboBoxEdit1";
+            this.comboBoxEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.comboBoxEdit1.Properties.Items.AddRange(new object[] {
+            "Day",
+            "Month",
+            "Quarter",
+            "Year"});
+            this.comboBoxEdit1.Size = new System.Drawing.Size(193, 20);
+            this.comboBoxEdit1.TabIndex = 3;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 13);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(50, 13);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "Start Date";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 58);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(44, 13);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "End Date";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(12, 103);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(38, 13);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "Interval";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 452);
+            this.Controls.Add(this.splitContainerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pivotGridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties.VistaTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties.VistaTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.comboBoxEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraPivotGrid.PivotGridControl pivotGridControl1;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField1;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField2;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField3;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField4;
+        private DevExpress.XtraPivotGrid.PivotGridField pivotGridField5;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.ComboBoxEdit comboBoxEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.DateEdit dateEdit2;
+        private DevExpress.XtraEditors.DateEdit dateEdit1;
+    }
+}
+
---
+++ VB/CustomDatesPivot/DateDataSourceWrapper.vb
@@ -0,0 +1,181 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.Collections
+
+Namespace CustomDatesPivot
+	Friend Class DateDataSourceWrapper
+		Implements ITypedList, IList
+		Public ReadOnly NestedList As IList
+		Public ReadOnly CustomDates As List(Of DateTime)
+		Public ReadOnly Property NestedTypedList() As ITypedList
+			Get
+				Return CType(NestedList, ITypedList)
+			End Get
+		End Property
+		Public Sub New(ByVal OriginalDatasource As ITypedList, ByVal dateFields As List(Of String), ByVal customDates As List(Of DateTime))
+			_DateFields = dateFields
+			Me.NestedList = CType(OriginalDatasource, IList)
+			Me.CustomDates = customDates
+		End Sub
+
+		Private ReadOnly _DateFields As List(Of String)
+
+
+
+		Private Class EmptyObjectPropertyDescriptor
+			Inherits PropertyDescriptor
+			Private ReadOnly _DateFields As List(Of String)
+			Public ReadOnly NestedDescriptor As PropertyDescriptor
+			Public Sub New(ByVal nestedDescriptor As PropertyDescriptor, ByVal dateFields As List(Of String))
+				MyBase.New(nestedDescriptor.Name, CType(New ArrayList(nestedDescriptor.Attributes).ToArray(GetType(Attribute)), Attribute()))
+				_DateFields = dateFields
+				Me.NestedDescriptor = nestedDescriptor
+			End Sub
+			Public Overrides Function CanResetValue(ByVal component As Object) As Boolean
+				Return False
+			End Function
+			Public Overrides ReadOnly Property ComponentType() As Type
+				Get
+					Return GetType(Object)
+				End Get
+			End Property
+			Public Overrides Function GetValue(ByVal component As Object) As Object
+				If TypeOf component Is DateTime Then
+					If _DateFields.Contains(NestedDescriptor.Name) Then
+						Return CDate(component)
+					End If
+					Return Nothing
+				Else
+					Return NestedDescriptor.GetValue(component)
+				End If
+			End Function
+			Public Overrides ReadOnly Property IsReadOnly() As Boolean
+				Get
+					Return True
+				End Get
+			End Property
+			Public Overrides ReadOnly Property PropertyType() As Type
+				Get
+					Return NestedDescriptor.PropertyType
+				End Get
+			End Property
+			Public Overrides Sub ResetValue(ByVal component As Object)
+				Throw New NotSupportedException("The method or operation is not implemented.")
+			End Sub
+			Public Overrides Sub SetValue(ByVal component As Object, ByVal value As Object)
+				Throw New NotSupportedException("The method or operation is not implemented.")
+			End Sub
+			Public Overrides Function ShouldSerializeValue(ByVal component As Object) As Boolean
+				Return True
+			End Function
+		End Class
+		Public Function GetItemProperties(ByVal listAccessors() As PropertyDescriptor) As PropertyDescriptorCollection Implements ITypedList.GetItemProperties
+			Dim result As New List(Of PropertyDescriptor)()
+			For Each pd As PropertyDescriptor In NestedTypedList.GetItemProperties(ExtractOriginalDescriptors(listAccessors))
+
+				result.Add(New EmptyObjectPropertyDescriptor(pd, _DateFields))
+			Next pd
+			Return New PropertyDescriptorCollection(result.ToArray())
+		End Function
+		Public Function GetListName(ByVal listAccessors() As PropertyDescriptor) As String Implements ITypedList.GetListName
+			Return NestedTypedList.GetListName(ExtractOriginalDescriptors(listAccessors))
+		End Function
+
+		Protected Shared Function ExtractOriginalDescriptors(ByVal listAccessors() As PropertyDescriptor) As PropertyDescriptor()
+			If listAccessors Is Nothing Then
+				Return Nothing
+			End If
+			Dim convertedDescriptors(listAccessors.Length - 1) As PropertyDescriptor
+			For i As Integer = 0 To convertedDescriptors.Length - 1
+				Dim d As PropertyDescriptor = listAccessors(i)
+				Dim c As EmptyObjectPropertyDescriptor = TryCast(d, EmptyObjectPropertyDescriptor)
+				If c IsNot Nothing Then
+					convertedDescriptors(i) = c.NestedDescriptor
+				Else
+					convertedDescriptors(i) = d
+				End If
+			Next i
+			Return convertedDescriptors
+		End Function
+		Public Function Add(ByVal value As Object) As Integer Implements IList.Add
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Function
+		Public Sub Clear() Implements IList.Clear
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Sub
+		Public Function Contains(ByVal value As Object) As Boolean Implements IList.Contains
+			If TypeOf value Is DateTime Then
+				Return CustomDates.Contains(CDate(value))
+			End If
+			Return NestedList.Contains(value)
+		End Function
+		Public Function IndexOf(ByVal value As Object) As Integer Implements IList.IndexOf
+			If TypeOf value Is DateTime AndAlso CustomDates.Contains(CDate(value)) Then
+				Return CustomDates.IndexOf(CDate(value))
+			End If
+			Dim nres As Integer = NestedList.IndexOf(value)
+			If nres < 0 Then
+				Return nres
+			End If
+			Return nres + CustomDates.Count
+		End Function
+		Public Sub Insert(ByVal index As Integer, ByVal value As Object) Implements IList.Insert
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Sub
+		Public ReadOnly Property IsFixedSize() As Boolean Implements IList.IsFixedSize
+			Get
+				Return True
+			End Get
+		End Property
+		Public ReadOnly Property IsReadOnly() As Boolean Implements IList.IsReadOnly
+			Get
+				Return True
+			End Get
+		End Property
+		Public Sub Remove(ByVal value As Object) Implements IList.Remove
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Sub
+		Public Sub RemoveAt(ByVal index As Integer) Implements IList.RemoveAt
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Sub
+		Default Public Property Item(ByVal index As Integer) As Object Implements IList.Item
+			Get
+				If index < CustomDates.Count Then
+					Return CustomDates(index)
+				Else
+					Return NestedList(index - CustomDates.Count)
+				End If
+			End Get
+			Set(ByVal value As Object)
+				Throw New NotSupportedException("The method or operation is not implemented.")
+			End Set
+		End Property
+		Public Sub CopyTo(ByVal array As Array, ByVal index As Integer) Implements System.Collections.ICollection.CopyTo
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Sub
+		Public ReadOnly Property Count() As Integer Implements System.Collections.ICollection.Count
+			Get
+				Return NestedList.Count + CustomDates.Count
+			End Get
+		End Property
+		Public ReadOnly Property IsSynchronized() As Boolean Implements System.Collections.ICollection.IsSynchronized
+			Get
+				Return False
+			End Get
+		End Property
+		Public ReadOnly Property SyncRoot() As Object Implements System.Collections.ICollection.SyncRoot
+			Get
+				Return NestedList.SyncRoot
+			End Get
+		End Property
+		Public Function GetEnumerator() As IEnumerator Implements System.Collections.IEnumerable.GetEnumerator
+			Throw New NotSupportedException("The method or operation is not implemented.")
+		End Function
+	End Class
+End Namespace
